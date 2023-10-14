15eedb05902bfd282221606cf6de0454272b33b9
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-10-14 12:20:38 +0100

    styling navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import Logo from '../assets/Logo.svg'
+import './Nav.css'
 function Nav() {
   return (
     <nav className='mainnavbar'>
+      <img src={Logo}/>
       <ul className='navbar_links'>
         <li className='navbar_link'><a href='/'>Home</a></li>
-        <li className='navbar_link'><a>About</a></li>
-        <li className='navbar_link'><a>Manu</a></li>
+        <li className='navbar_link'><a href='about'>About</a></li>
+        <li className='navbar_link'><a href='menu'>Menu</a></li>
         <li className='navbar_link'><a href='reservations'>Reservations</a></li>
-        <li className='navbar_link'><a>Order Online</a></li>
-        <li className='navbar_link'><a>Login</a></li>
+        <li className='navbar_link'><a href='orderonline'>Order Online</a></li>
+        <li className='navbar_link'><a href='login'>Login</a></li>
       </ul>
     </nav>
   )
