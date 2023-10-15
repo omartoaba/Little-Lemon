8bfc2e0c568dac88e2a0880415d9d4fce7fd20e1
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-10-15 13:55:45 +0100

    adding reserve form

--- src/Components/Reservations.jsx
+++ src/Components/Reservations.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Reservations.css'
+import {Inside,Outside,Adults,Children,Clock,Date,Customer,Mail,Phone} from './imagesimports'
 function Reservations() {
+  const [location,setlocation] = useState('inside');
+  const changelocation = (event) => {
+        setlocation(event.target.id);
+  }
   return (
-    <div>Reservations</div>
+    <div className='reservations_maincontainer'>
+      <h2>Reserve a Table</h2>
+      <hr/>
+        <h3>Where do you want to Eat?</h3>
+      <div className="reservation_location">
+        <button className={location === 'inside' ? 'reservations_location-option active' : 'reservations_location-option'} 
+          onClick={changelocation} id='inside'>
+        <input type="radio" name='location' hidden/>
+        <img src={Inside}/>
+        <label htmlFor="inside">Inside</label>
+        </button>
+        <button className={location === 'outside' ? 'reservations_location-option active' : 'reservations_location-option'}
+            onClick={changelocation} id='outside'>
+          <input  type="radio" name='location' hidden/>
+          <img src={Outside}/>
+          <label htmlFor="outside">Outside</label>
+        </button>
+      </div>
+      <div className="reservation_option">
+          <img src={Adults} className='reservation_option-img'/>
+          <input className='reservation_option-input'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Children} className='reservation_option-img'/>
+          <input className='reservation_option-input'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Clock}className='reservation_option-img'/>
+          <input className='reservation_option-input'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Date}className='reservation_option-img'/>
+          <input className='reservation_option-input'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Customer}className='reservation_option-img'/>
+          <input className='reservation_option-input'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Phone}className='reservation_option-img'/>
+          <input className='reservation_option-input' type='phone'/>
+        </div>
+        <div className="reservation_option">
+          <img src={Mail}className='reservation_option-img'/>
+          <input className='reservation_option-input' type='email'/>
+        </div>
+        <div className="reservarionsfooter">
+        <button className='primaryButton'>Reserve Table</button>
+        <button className='secondaryButton'>Cancel</button>
+        </div>
+    </div>
   )
 }
 
---
+++ src/Components/imagesimports.js
@@ -0,0 +1,22 @@
+import Inside from '../assets/inside.png'
+import Outside from '../assets/outside.png'
+import Adults from '../assets/couple.png'
+import Children from '../assets/children.png'
+import Clock from '../assets/clock.png'
+import Date from '../assets/event.png'
+import Customer from '../assets/man.png'
+import Mail from '../assets/mail.png'
+import Phone from '../assets/phone.png'
+
+
+export {
+    Inside,
+    Outside,
+    Adults,
+    Children,
+    Clock,
+    Date,
+    Customer,
+    Mail,
+    Phone
+}
